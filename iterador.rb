8489c88b2c83f3522d8c7192991e1825a269dd60
marlen713 <ma000161@gmail.com>
2022-03-25 19:16:37 -0300

    fineshead: iterador.rb

---
+++ iterador.rb
@@ -0,0 +1,15 @@
+#En el siguiente código, reemplazar la instrucción while por times dentro del programa
+#llamado iterador.rb.
+
+# el siguiente codigo
+#i = 0
+#while i < 50
+#puts "Iteración #{i}"
+#i = i + 1
+#end
+
+50.times do |i|
+  puts "Iteration #{i}" 
+end
+
+#50.times {|i| puts "iteration #{i}"} 
