55690cb8cbc92b1b1b2cad8b6cd9a9e4629006c3
marlen713 <ma000161@gmail.com>
2022-03-28 16:40:17 -0300

    finishead: cuenta_regresiva.rb

---
+++ cuenta_regresiva.rb
@@ -0,0 +1,18 @@
+# En el siguiente código, reemplaza la instrucción until por while dentro del programa
+# llamado cuenta_regresiva.rb.
+
+# puts 'Ingrese un número para comenzar la cuenta:'
+# cuenta_regresiva = ARGV[0].to_i
+# puts "Contando desde #{cuenta_regresiva}..."
+# until cuenta_regresiva < 0
+#     puts cuenta_regresiva
+#     cuenta_regresiva -= 1
+# end
+
+puts 'Ingrese un número para comenzar la cuenta:'
+cuenta_regresiva = gets.to_i
+puts "Contando desde #{cuenta_regresiva}..."
+while cuenta_regresiva >= 0
+    puts cuenta_regresiva
+    cuenta_regresiva -= 1
+end
